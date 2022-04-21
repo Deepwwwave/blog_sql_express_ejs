5c2e5fff657e9bfec6da3357f27f0c73ec677bac
deepwwwave <deepwwwave@gmail.com>
2022-04-21 17:35:03 +0200

    update server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+import {fileURLToPath} from "url";
+import path from "path";
 import express from "express";
-const app = express()
-
 import mysql from 'mysql';
+
+const app = express()
 const PORT = 9000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filname);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname + 'public')))
+
+// const pool = mysql.createPool({
+    
+// })
+
+app.listen( PORT, ()=>{
+    console.log(`Listening at http://localhost:${PORT}`)
+})
