e9b685e24e3d5d77802a3be27b044ca0535458f3
deepwwwave <deepwwwave@gmail.com>
2022-04-21 16:56:38 +0200

    fisrt commit

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express()
+
+import mysql from 'mysql';
+const PORT = 9000
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filname);
+
